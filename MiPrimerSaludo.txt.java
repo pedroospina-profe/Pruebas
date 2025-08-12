77ce1a2b13275ba7a2244af0465155b5627d72f4
Pedro Daniel Ospina <pedro_ospina@cun.edu.co>
2025-08-12 12:44:00 -0500

    Add files via upload

---
+++ MiPrimerSaludo.txt.java
@@ -0,0 +1,7 @@
+// Este programa imprime un saludo personalizado
+public class MiPrimerSaludo {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, soy [Tu Nombre] y este es mi primer programa en Java!");
+        System.out.println("Estoy emocionado/a de aprender programación con Java.");
+    }
+}
